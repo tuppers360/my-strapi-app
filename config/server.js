ab8ded46f6981c8842860d9d495e2c7ce1caad3a
tuppers360 <tuppers360@live.co.uk>
2021-01-01 15:29:11 +0000

    Initial Commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '12e99b1d88357c781e34aeafbd26aa65'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'c8f3de5e-692a-4f64-8266-c0d3b769a575'
+};
